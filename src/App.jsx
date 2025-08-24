415ac81afa1a7ffab0fdd05dfc91f1ea732d83df
kiddydprc <46512@virtual.prc.ac.th>
2025-08-24 19:10:56 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       fontFamily: "Arial, sans-serif"
     }}>
       <h1>AI SpineCheck</h1>
-      <p>Upload a photo of your back for scoliosis screening.</p>
+      <p>Upload a phfvfvfvfvfvfvfvfvfvfvfvboto of your back for scoliosis screening.</p>
 
       <button style={{
         padding: "12px 24px",
